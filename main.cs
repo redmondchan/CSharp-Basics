ad4fb082254d64cda4adceb5750af07342fd55a4
Redmond Chan <redmondchan92@gmail.com>
2019-12-14 19:41:59 +0000

    Created variable and console logged variable

--- main.cs
+++ main.cs
@@ -3,5 +3,8 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+
+    int x = 100; 
+    Console.WriteLine("I have {0} dogs", x);
   }
 }
