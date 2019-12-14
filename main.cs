6ce9a6fc29e3a8ddd77edea10780e4884f2c3f3f
Redmond Chan <redmondchan92@gmail.com>
2019-12-14 20:22:44 +0000

    Added string interpolation

--- main.cs
+++ main.cs
@@ -2,12 +2,17 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+
     Console.WriteLine ("Hello World");
 
     int x = 100; 
-    Console.WriteLine("I have {0} dogs", x);
+    var y = "dogs";
+    Console.WriteLine("I have {0} {1}", x, y);
+    Console.WriteLine($"I have {x} {y}");
 
     var yourName = Console.ReadLine();
     Console.WriteLine("Your name is {0}", yourName);
+
+
   }
 }
